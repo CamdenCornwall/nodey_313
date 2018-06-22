9de595eb08e6c72cbd50f14800a21580b90fb349
Camden C. Cornwall <camdenc.cornwall@gmail.com>
2018-06-21 18:08:33 -0600

    postage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
+var url = require('url');
 const PORT = process.env.PORT || 5000;
 
 express()
@@ -8,7 +9,39 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/submit-postage', function(req, res) {postageCalc(req, res);})
   .get('/postage', (req, res) => res.render('pages/postage'))
-  .get('/submit-postage', (req, res) => res.send(cool()))
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+function postageCalc(request, response){
+    var requestUrl = url.parse(request.url, true);
+	console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+        var cost = 0;
+        var weight = Number(requestUrl.query.weight);
+	var mail = requestUrl.query.mail;
+        
+        
+        
+        if(mail == "stamped") {
+            cost += 5;
+        }
+        else if(mail == "metered") {
+            cost += 10;
+        }
+        else if(mail == "flats") {
+            cost += 15;
+        }
+        else if(mail == "retail") {
+            cost += 20;
+        }
+        var result = weight * 5;
+        var result2 = result + cost;
+        
+        var param = {mail: mail, weight: weight, totalCost: result2};
+        
+        response.render('pages/postage_total', param);
+        
+
+}
