24163de8b99f91666ce0abf8de87f067d5f11083
Camden C. Cornwall <camdenc.cornwall@gmail.com>
2018-07-05 23:19:02 -0600

    Got the system in working order, plus looks cool

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ const cool = require('cool-ascii-faces');
 var url = require('url');
 const PORT = process.env.PORT || 5000;
 
-express()
-  //.use(express.static(path.join(__dirname, 'public')))
+app()
+  .use(app.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .engine('html', require('ejs').renderFile)
   .set('view engine', 'ejs')
