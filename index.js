b1386fb8367c0ef0971458f77c554d29eab647e6
Camden C. Cornwall <camdenc.cornwall@gmail.com>
2018-07-05 23:17:04 -0600

    Got the system in working order, plus looks cool

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ var url = require('url');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  //.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .engine('html', require('ejs').renderFile)
   .set('view engine', 'ejs')
